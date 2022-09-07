4252b8b60e754f9fdd4b8c5034e3e8c146c66f41
AnaClara81 <anaceceiza81@gmail.com>
2022-09-06 22:12:41 -0300

    pre-entrega2

--- Complementario2/index.js
+++ Complementario2/index.js
@@ -2,49 +2,65 @@ const recetasPollo = [
     {id: 1, nombre:"pollo con arroz", ingrediente:"arroz"},
     {id: 2, nombre:"pollo al horno con papas",ingrediente:"papas"},
     {id: 3, nombre:"milanesas de pollo con ensalada", ingrediente:"ensalada"},
-    {id: 3, nombre:"pollo arrollado con pure", ingrediente:"jamon"},
+    {id: 4, nombre:"pollo arrollado con pure", ingrediente:"jamon"},
     ];
-    let ingrediente = prompt("Ingrese el ingrediente a buscar arroz, papas , ensalada o jamon");
 
-    let filtradosP = recetasPollo.filter(item => item.ingrediente === ingrediente);
-    console.log(filtradosP);
-    
-     /*  const recetasCarne = [
-        {id: 1, nombre:"bifes con verduras", ingrediente:"verduras"},
-        {id: 2, nombre:"carne al horno con papas",ingrediente:"papas"},
-        {id: 3, nombre:"milanesas napolitana con ensalada", ingrediente:"ensalada"},
-        {id: 3, nombre:"estofado", ingrediente:"zapallo"},
-        ];
-        for (const item of recetasCarne){
-        //console.log(item.nombre);*/
+
+ const recetasCarne = [
+      {id: 1, nombre:"bifes con verduras", ingrediente:"verduras"},
+      {id: 2, nombre:"carne al horno con papas",ingrediente:"papas"},
+      {id: 3, nombre:"milanesas napolitana con ensalada", ingrediente:"ensalada"},
+      {id: 4, nombre:"estofado", ingrediente:"zapallo"},
+      ];
+      elegirIngrediente();
+      //elegirReceta();  
+     
+        
+function elegirIngrediente(){  // muestra por alert el arreglo elegido
+  let carne = "carne";
+  let pollo = "pollo";
+  let receta =prompt("Ingrese el ingrediente principal de su receta" + pollo + "o" + carne);
+  let mensaje1 = ( Object.values(recetasCarne));
+  let mensaje2 = (Object.values(recetasPollo));
+        
+ if (receta === carne){
+   alert(JSON.stringify( mensaje1));
+   // console.log(Object.values(recetasCarne));
+  }else if (receta === pollo) { 
+        alert(JSON.stringify( mensaje2));
+       // console.log(Object.values(recetasPollo));
+  }else{
+      alert("Receta no encontrada");
+ }
+
+ };
+// una vez elegida la opcion pollo o carne tendria que dar la opcion de elegir la receta de cada cual ... 
+// y mostrar la funcion correspondiente  elegirRecetaCarne() / elegirRecetaPollo()
+
+
+
+
+
+function elegirRecetaCarne(){
    
+   let ingrediente =prompt("Ingrese el ingrediente a buscar verduras, papas , ensalada o zapallo");
 
+   let filtradosP = recetasCarne.filter(item => item.ingrediente === ingrediente);
+    filtradosP.forEach(item =>{
+    alert(` con sus ingredientes puede cocinar: ${item.nombre}`);
+    })
+  };
+
+  function elegirRecetaPollo(){
+   
+    let ingrediente =prompt("Ingrese el ingrediente a buscar arroz, papas , ensalada o jamon");
  
-/*
-/*for(const item of recetasCarne){
-  //console.log(item);*/
-
-/*let receta = prompt("Ingrese el ingrediente principal de la receta a buscar pollo/carne");
-
-let mensaje1 = `receta con carne ${recetasCarne.nombre}`;
-let mensaje2 = `receta con pollo ${recetasPollo.nombre}`;
+    let filtradosP = recetasPollo.filter(item => item.ingrediente === ingrediente);
+     filtradosP.forEach(item =>{
+     alert(` con sus ingredientes puede cocinar: ${item.nombre}`);
+     })
+   };
+ 
 
 
-//while (receta != "ESC") {
-  switch (receta){
-    case "carne":
-        alert(mensaje1);
-    //console.log("Estas  son las recetas con carne " + recetasCarne);
-      break;
-
-    case "pollo":
-        alert(mensaje2);
-    console.log("Estas  son las recetas con carne " +  recetasPollo);
-      break;
-
-    default:
-      console.log("Receta no encontrada");
-      break;
-    };*/
-  
 
