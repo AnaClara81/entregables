1f6e7487311ffadd7ec9ada50e29a943b9a50665
AnaClara81 <anaceceiza81@gmail.com>
2022-09-10 18:55:37 -0300

    borrador complementario3

--- Complementario2/index.js
+++ Complementario2/index.js
@@ -1,3 +1,12 @@
+const recetasCarne = [
+  {id: 1, nombre:"Bifes con verduras", ingrediente:"Verduras"},
+  {id: 2, nombre:"Carne al horno con papas",ingrediente:"Papas"},
+  {id: 3, nombre:"Milanesas napolitana con ensalada", ingrediente:"Ensalada"},
+  {id: 4, nombre:"Estofado", ingrediente:"Zapallo"},
+  ];
+
+
+
 const recetasPollo = [
     {id: 1, nombre:"pollo con arroz", ingrediente:"arroz"},
     {id: 2, nombre:"pollo al horno con papas",ingrediente:"papas"},
@@ -5,16 +14,10 @@ const recetasPollo = [
     {id: 4, nombre:"pollo arrollado con pure", ingrediente:"jamon"},
     ];
 
-
- const recetasCarne = [
-      {id: 1, nombre:"bifes con verduras", ingrediente:"verduras"},
-      {id: 2, nombre:"carne al horno con papas",ingrediente:"papas"},
-      {id: 3, nombre:"milanesas napolitana con ensalada", ingrediente:"ensalada"},
-      {id: 4, nombre:"estofado", ingrediente:"zapallo"},
-      ];
-
-
- elegirIngrediente();
+ 
+ 
+ 
+    elegirIngrediente();
        
      
         
@@ -22,43 +25,72 @@ function elegirIngrediente(){
   let carne = "carne";
   let pollo = "pollo";
   let receta =prompt("Ingrese el ingrediente principal de su receta " + pollo + " o " + carne);
-  let mensaje1 = ( Object.values(recetasCarne));
-  let mensaje2 = (Object.values(recetasPollo));
-        
- if(receta === carne){
+ 
+  if(receta === pollo){ 
+    elegirRecetaPollo();
+
+}else if(receta === carne){
   elegirRecetaCarne();
-}else if(receta === pollo){ 
-  elegirRecetaPollo();
+
 }else{
    alert("Receta no encontrada");
  }
 };
 
+
 function elegirRecetaCarne(){
-   
+   let contenedor1 = document.getElementById("contenedor1");
    let ingrediente =prompt("Con que ingrediente desea acompañar la carne? verduras, papas , ensalada o zapallo");
 
-   let filtradosP = recetasCarne.filter(item => item.ingrediente === ingrediente);
-    filtradosP.forEach(item =>{
-    alert(`Con carne y ${ingrediente} puede cocinar: ${item.nombre}`);
-    })
+   let filtradosCarne = recetasCarne.filter(item => item.ingrediente === ingrediente);
+   for (const receta of filtradosCarne) {
+    let div = document.createElement("div");
+ div.innerHTML = ` <h3>Podria preparar con carne</h3>
+                   <h3> ID: ${receta.id}</h3>
+                  <p>Producto: ${receta.nombre}</p>
+                  <b>${receta.ingrediente}</b>`;
+ contenedor1.append(div);
+   }
   };
 
+
+
 function elegirRecetaPollo(){
-   
+   let contenedor2 = document.getElementById("contenedor2");
     let ingrediente =prompt("Con que ingrediente desea acompañar la pollo? arroz, papas , ensalada o jamon");
  
-    let filtradosP = recetasPollo.filter(item => item.ingrediente === ingrediente);
-     filtradosP.forEach(item =>{
-     alert(`Con pollo y ${ingrediente}  puede cocinar: ${item.nombre}`);
-     })
-   };
+    let filtradosPollo = recetasPollo.filter(item => item.ingrediente === ingrediente);
+    for (const receta of filtradosPollo) {
+     let div = document.createElement("div");
+  div.innerHTML = `<h3>Podria preparar con pollo</h3>
+                   <h3> ID: ${receta.id}</h3>
+                   <p>Producto: ${receta.nombre}</p>
+                   <b>${receta.ingrediente}</b>`;
+  contenedor2.append(div);
+    }
+ };
  
-
-  
+let contenedorVacuna = document.getElementById("contenedorVacuna");
+for (const recetas of recetasCarne) {
+  let li = document.createElement("li");
+  li.innerHTML = `<h4>id:${recetas.id}</h4>
+                   <h3>Receta:${recetas.nombre}</h3>
+                   <h5>Ingrediente:${recetas.ingrediente}</h5>`
+  contenedorVacuna.append(li);
+};
+ 
+ let contenedorPollo = document.getElementById("contenedorPollo");
+    for (const recetas of recetasPollo) {
+      let li = document.createElement("li");
+      li.innerHTML = `<h4>id:${recetas.id}</h4>
+                       <h3>Receta:${recetas.nombre}</h3>
+                       <h5>Ingrediente:${recetas.ingrediente}</h5>`
+      contenedorPollo.append(li);
+     
+    }
    
    
-   let ingredienteUno = (prompt("Ingrese proteina (tipo de carne)")) ;
+/*    let ingredienteUno = (prompt("Ingrese proteina (tipo de carne)")) ;
    let cantidadUno = parseInt(prompt("Ingrese la cantidad de proteina")) ;
    let ingredienteDos = (prompt("Ingrese carbohidratos cereales/ vegetales")) ;
    let cantidadDos = parseInt(prompt("Ingrese la cantidad de carbohidratos")) ;
@@ -87,7 +119,7 @@ function elegirRecetaPollo(){
    }
    };
    
-   
+  }
    
    cantPorciones();
-
+ */
